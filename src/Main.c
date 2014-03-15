d26e839bc88298d3687867ccc65c82630be1f609
Salma-El-Alaoui <s.elalaouitalibi@gmail.com>
2014-03-15 20:25:07 +0100

    2 threads a chaque tour de boucle

--- src/Main.c
+++ src/Main.c
@@ -10,35 +10,12 @@
 #include <stdint.h>
 #include <math.h>
 #include <inttypes.h>
-#include<pthread.h>
-#include<unistd.h>
+#include <pthread.h>
+#include <unistd.h>
 #include "Main.h"
 
-int main()
-{
-	
-	//printf("%d",is_prime(23));
-	//printf("\n");
-	//print_prime_factors(92);
-	 FILE * fichier =fopen ("numbers.txt", "r");
-	uint64_t p;
-	while(fscanf(fichier,"%llu",&p)!=EOF)
-	{
-		int c1, c2;
-		pthread_t t1,t2;
-
-		c1 =pthread_create(&t1,NULL,print_prime_factors,NULL);
-		c2 =pthread_create(&t2,NULL,print_prime_factors,NULL);
-		
-		c1 =pthread_join(t1,NULL);
-		c2 =pthread_join(t2,NULL);
-		
-		print_prime_factors(p);
-		printf("\n");
-		
-	}
-	
-}
+//pour éviter de réouvrir le fichier plusieurs fois.
+static FILE * fichier =fopen ("numbers.txt", "r");
 
 int is_prime(uint64_t p)
 
@@ -61,7 +38,7 @@ void print_prime_factors(uint64_t n)
 	
 	uint64_t i;
 	printf("%"PRIu64,n);
-	printf(":");
+	printf(": ");
 	for(i=2; i<=n ; i++)
 	{
 		
@@ -77,10 +54,63 @@ void print_prime_factors(uint64_t n)
 		}
 		
 	}
+	
 
 }
 
+static void * start_routine(void * arg)
+{
+	uint64_t n= (uint64_t)arg;
+	print_prime_factors(n);
+	printf("\n");
+	pthread_exit(NULL);
+}
 
+int main()
+{
+	
+	//printf("%d",is_prime(23));
+	//printf("\n");
+	//print_prime_factors(92);
+	uint64_t p;
+	while(fscanf(fichier,"%llu",&p)!=EOF)
+	{
+		
+	
+		int crdu;
+		pthread_t t1,t2;
+
+		crdu=pthread_create(&t1,NULL,start_routine,(void *)p);
+		if(crdu !=0)
+		{
+			exit(-1);
+		}
+		crdu=pthread_create(&t2,NULL,start_routine,(void *)p);
+		if(crdu !=0)
+		{
+			exit(-1);
+		}
+
+		//attente des threads
+		crdu=pthread_join(t1,NULL);
+		if(crdu !=0)
+		{
+			exit(-1);
+		}
+		crdu=pthread_join(t2,NULL);
+		if(crdu !=0)
+		{
+			exit(-1);
+		}
+		
+		/*print_prime_factors(p);
+		printf("\n");
+		*/
+	}
+		
+}
+	
+	
 
 
 
